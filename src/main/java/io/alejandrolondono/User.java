17ee4a3877d7cfe64d12ed8d3e2b04cd58815901
Alejandro Londono <alondono11@gmail.com>
2016-05-09 20:43:59 -0400

    adding user and user unit tests to ATM

--- src/main/java/io/alejandrolondono/User.java
+++ src/main/java/io/alejandrolondono/User.java
@@ -8,10 +8,68 @@ import java.util.UUID;
  */
 public class User {
 
-    ArrayList<Account> accounts = new ArrayList<Account>();
-    String first;
-    String last;
-    String userId = "U"+UUID.randomUUID().toString();
+    private ArrayList<Account> accounts = new ArrayList<Account>();
+    private String first;
+    private String last;
+    private int pin;
+    private String userId = "U"+UUID.randomUUID().toString();
+
+
+    public User(String first, String last){
+        this.first = first;
+        this.last = last;
+
+    }
+
+    public void addAccount(Account acct){
+        ArrayList<Account> accounts = this.getAccounts();
+        accounts.add(acct);
+    }
+
+    /*
+    getters and setters below
+     */
+
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+//    public void setAccounts(ArrayList<Account> accounts) {
+//        this.accounts = accounts;
+//    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
 
 
---
+++ src/test/java/io/alejandrolondono/UserSpec.java
@@ -0,0 +1,17 @@
+package io.alejandrolondono;
+
+import org.junit.Before;
+
+/**
+ * Created by alejandrolondono on 5/9/16.
+ */
+public class UserSpec {
+
+    User newUser = new User("Alex", "London");
+
+    @Before
+    public void Sandbox(){
+
+    }
+
+}
